f1992220663e8a156c4369940f147d0d087580d8
Sam Rodenburg <ps198115@summacollege.nl>
2021-04-06 14:50:01 +0200

    blackjack scherm

---
+++ StonksCasino/StonksCasino/Views/blackjack/BlackjackWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using StonksCasino.classes.Main;
+
+namespace StonksCasino.Views.blackjack
+{
+    /// <summary>
+    /// Interaction logic for BlackjackWindow.xaml
+    /// </summary>
+    public partial class BlackjackWindow : Window
+    {
+        public User User { get; set; }
+        public BlackjackWindow()
+        {
+            Account();
+            DataContext = this;
+            InitializeComponent();
+        }
+        private void Account()
+        {
+            DataTable dataTable = Database.Accounts();
+            string Name = dataTable.Rows[0]["Gebruikersnaam"].ToString();
+            int Tokens = (int)dataTable.Rows[0]["Token"];
+            User = new User(Name, Tokens);
+        }
+    
+}
+}
