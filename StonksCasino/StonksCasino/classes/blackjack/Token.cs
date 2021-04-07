983e5d641ea1f1b49a454d7a620c2595a7066377
Sam Rodenburg <ps198115@summacollege.nl>
2021-04-07 15:17:00 +0200

    Deal functie deels af MessegeBox.Show

---
+++ StonksCasino/StonksCasino/classes/blackjack/Token.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StonksCasino.classes.blackjack
+{
+    public class Token : PropertyChange
+    {
+        private int _aantal;
+
+        public int MyAantal
+        {
+            get { return _aantal; }
+            set { _aantal = value; }
+        }
+
+        public void GeefMelding()
+        {
+            MessageBox.Show($"Het aantal ingezette Tokens: { MyAantal }");
+        }
+    }
+}
