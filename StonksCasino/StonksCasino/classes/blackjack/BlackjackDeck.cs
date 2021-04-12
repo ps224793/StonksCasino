67b0c45f3a42403f3badc526b5a3c8c257c5d0fc
Sam Rodenburg <ps198115@summacollege.nl>
2021-04-12 10:05:21 +0200

    Function buttons af

---
+++ StonksCasino/StonksCasino/classes/blackjack/BlackjackDeck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StonksCasino.classes.Main;
+
+namespace StonksCasino.classes.blackjack
+{
+    class BlackjackDeck : CardDeck
+    {
+        public BlackjackDeck()
+        {
+            AssembleDeck();
+            AssembleDeck();
+        }
+    }
+}
