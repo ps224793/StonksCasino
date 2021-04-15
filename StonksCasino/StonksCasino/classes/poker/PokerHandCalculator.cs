504a026f9bd7f4287a4a79b8bb666f820712f92c
LuukPaans <47414476+LuukPaans@users.noreply.github.com>
2021-04-15 11:13:38 +0200

    start of PokerHandValue

---
+++ StonksCasino/StonksCasino/classes/poker/PokerHandCalculator.cs
@@ -0,0 +1,30 @@
+﻿using StonksCasino.classes.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StonksCasino.classes.poker
+{
+    class PokerHandCalculator
+    {
+        public static PokerHandValue GetHandValue(List<Card> hand, List<Card> table)
+        {
+            List<Card> cards = new List<Card>();
+            cards.AddRange(hand);
+            cards.AddRange(table);
+
+
+
+            return null;
+        }
+
+        private bool RoyalFlush(List<Card> cards)
+        {
+            
+
+            return false;
+        }
+    }
+}
