4b33edde4defbeab481061b8d2f336c105008dcb
Sam Rodenburg <ps198115@summacollege.nl>
2021-05-20 11:48:16 +0200

    Verhogen verlagen button aan gewerkt

--- StonksCasino/StonksCasino/Views/slotmachine/SlotmachineWindow.xaml.cs
+++ StonksCasino/StonksCasino/Views/slotmachine/SlotmachineWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using StonksCasino.classes.blackjack;
 using StonksCasino.classes.Main;
+using StonksCasino.classes.Roulette;
+using StonksCasino.classes.Slotmachine;
 using StonksCasino.Views.main;
 
 namespace StonksCasino.Views.slotmachine
@@ -32,12 +34,34 @@ namespace StonksCasino.Views.slotmachine
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
+        private Database _database = new Database();
+
+        public Database MyDatabase
+        {
+            get { return _database; }
+            set { _database = value; }
+        }
+        private void accountrefresh()
+        {
+            DataTable dataTable = Database.Accounts();
+            _Tokens = (int)dataTable.Rows[0]["Token"];
+            user.MyTokens = _Tokens;
+            long Time = (long)dataTable.Rows[0]["Timestamp"];
+            if (Time != Properties.Settings.Default.Timestamp)
+            {
+                _database.MyLogout = false;
+                Application.Current.Shutdown();
+            }
+        }
 
         public User user { get; set; }
         public User User { get; set; }
         int _Tokens;
 
         DispatcherTimer computertimer = new DispatcherTimer();
+
+        public int Beurt = 100;
+        public int _betAmount;
 
         public SlotmachineWindow(User user)
         {
@@ -61,6 +85,7 @@ namespace StonksCasino.Views.slotmachine
             _Tokens = (int)dataTable.Rows[0]["Token"];
             user.MyTokens = _Tokens;
         }
+
         private void Bibliotheek_Click(object sender, EventArgs e)
         {
 
@@ -92,11 +117,38 @@ namespace StonksCasino.Views.slotmachine
         {
             HelpTab();
         }
-
         private void HelpTab()
         {
             HelpWindow roulette = new HelpWindow();
             roulette.Show();
+        }
+
+        private Bet _myamount = new Bet();
+
+        public Bet MyAmount
+        {
+            get { return _myamount; }
+            set { _myamount = value; }
+        }
+
+        public void Verhogen_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable data = Database.Tokensadd(100);
+            accountrefresh();
+        }
+
+        private void Verlagen_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable data = Database.Tokensremove(100);
+            accountrefresh();
+        }
+
+        private Slotmachine _slotmachine;
+
+        public Slotmachine Slotmachine
+        {
+            get { return _slotmachine; }
+            set { _slotmachine = value; OnPropertyChanged(); }
         }
     }
 }
